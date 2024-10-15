763d883961ae38c50e2686b9362cfc7b525743e0
rafael_pm0 <rafaelpmedeiros00@gmail.com>
2024-10-15 13:11:26 -0300

    estilizacao inicial sidebar e uso do contexto global

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -6,16 +6,19 @@ import {useGlobalState} from '@/app/context/globalProvider'
 function SideBar(){
 
     const {theme} = useGlobalState();
-    console.log(theme);
+  
 
-    return(<SideBarStyled>
+    return(<SideBarStyled theme={theme}>
         SideBar
     </SideBarStyled>)
 }
 
 const SideBarStyled = styled.nav`
-
-
+    width: ${(props) => props.theme.sidebarWidth};
+    position:relative;
+    background-color: ${(props) => props.theme.colorBg2};
+    border: 2px solid ${(props) => props.theme.borderColor2};
+    border-radius: 1rem
 
 `
 
