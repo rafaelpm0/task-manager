4707390b2ee65e9a22993e328982f7e9e293e3b5
rafael_pm0 <rafaelpmedeiros00@gmail.com>
2024-10-15 13:57:49 -0300

    injecao dos menus e seus incones

--- app/Components/Sidebar/Sidebar.tsx
+++ app/Components/Sidebar/Sidebar.tsx
@@ -1,30 +1,46 @@
-"use client"
-import React from 'react'
-import styled from 'styled-components';
-import {useGlobalState} from '@/app/context/globalProvider'
+"use client";
+import React from "react";
+import styled from "styled-components";
+import { useGlobalState } from "@/app/context/globalProvider";
+import  menu  from "@/app/utils/menu";
+import Link from "next/link";
 
-function SideBar(){
+function SideBar() {
+  const { theme } = useGlobalState();
 
-    const {theme} = useGlobalState();
-  
-
-    return(<SideBarStyled theme={theme}>
-        <div className="profile">
-            <div className="profile-overlay">
-                <img src="/avatar1.png" alt="profile" width={70} height={70}/>
-            </div>
+  return (
+    <SideBarStyled theme={theme}>
+      <div className="profile">
+        <div className="profile-overlay">
+          <div className="image">
+            <img src="/avatar1.png" alt="profile" width={70} height={70} />
+          </div>
+          <h1>
+            <span>Sin</span>
+            <span>Rostro</span>
+          </h1>
         </div>
-    </SideBarStyled>)
+        <ul className="nav-items">
+        {menu.map((item: any)=>{
+            return <li className="item">
+                {item.icon}
+                <Link href={item.link}>{item.title}</Link>
+            </li>
+        })
+
+        }
+        </ul>
+      </div>
+    </SideBarStyled>
+  );
 }
 
 const SideBarStyled = styled.nav`
-    width: ${(props) => props.theme.sidebarWidth};
-    position:relative;
-    background-color: ${(props) => props.theme.colorBg2};
-    border: 2px solid ${(props) => props.theme.borderColor2};
-    border-radius: 1rem
-
-`
+  width: ${(props) => props.theme.sidebarWidth};
+  position: relative;
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
+`;
 
 export default SideBar;
-
